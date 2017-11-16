eb412893199c87852e1ab8f35ec952df60b37bb1
awhedon <32019899+awhedon@users.noreply.github.com>
2017-11-16 16:22:59 -0700

    Add files via upload

---
+++ data_preprocessing_template.py
@@ -0,0 +1,39 @@
+%reset -f
+
+# Importing the libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+# Importing the dataset
+os.getcwd()
+os.chdir('C:\\Users\\alexa\\Desktop\\Machine Learning A-Z\\Part 1 - Data Preprocessing')
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,3].values
+
+# Taking care of the missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+labelencoder_Y = LabelEncoder()
+y = labelencoder_Y.fit_transform(y)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state=0)
+
+# Feature Sclaing
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
